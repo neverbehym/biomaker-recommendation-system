dff8e487eb543968ddf58dfafa0a98d5b59361e3
Yiming Huang <31046548+neverbehym@users.noreply.github.com>
2022-11-24 15:37:21 -0500

    Update BIM.R
    Commentary &  results writing

--- scripts/BIM.R
+++ scripts/BIM.R
@@ -1,24 +1,24 @@
 
 ############# read a pool of candidate biomarker panels #############
 
-panel_log <- read.table('../data/CandidateBiomarkerList.csv', sep=",", header = TRUE,stringsAsFactors = FALSE)
+panel_log <- read.table('../results/CandidateBiomarkerList.csv', sep=",", header = TRUE,stringsAsFactors = FALSE,row.names=1)
 
 panel_list <- strsplit(panel_log$genes,';')
 
 biomarker_union<-unique(unlist(panel_list))
 length(biomarker_union)
 
-### read gene expression data and multi-class (cellular states) labels ###
+############# read gene expression data and multi-class (cellular states) labels #############
 m_express <- as.matrix(read.table("../data/NicolasGeneExpression_processed.csv", sep=",", header = TRUE, stringsAsFactors = FALSE,row.names = 1))
 multiclass_labels <- read.table('../data/MultiClassLabel.csv',sep=',',stringsAsFactors = FALSE,row.names = 1)
 num_classes <- length(unique(multiclass_labels$classID))
 
-############# 1) performance score #############
+############# compute the evaluation metrics #############
 
+### performance score
 panel_log$performance
 
-############# 2) frequency score #############
-
+### frequency score
 s_freqPerbio<- rep(0,length(biomarker_union))
 names(s_freqPerbio)<-biomarker_union
 for (i in 1:length(panel_list))
@@ -30,14 +30,13 @@ names(s_freqPerPanel)<-1:length(panel_list)
 for (i in 1:length(panel_list))
   s_freqPerPanel[i]<-mean(s_freqPerbio[panel_list[[i]]])
 
-############ 3) effect size score #######
-
-cohen_d<- matrix(0,nrow=length(biomarker_union),ncol = 10)
+###effect size score
+cohen_d<- matrix(0,nrow=length(biomarker_union),ncol = num_classes)
 for (i in 1:num_classes){
   exp_groupA<-m_express[biomarker_union,which(class==i)]
   min_d<-rep(1000,length(biomarker_union))
-  for (j in c(1:(i-1),(i+1):10)){
-    if((i==1 & j<2) | (i==10 & j>9))
+  for (j in c(1:(i-1),(i+1):num_classes)){
+    if((i==1 & j<2) | (i==num_classes & j>(num_classes-1)))
       next
     exp_groupB<-m_express[biomarker_union,which(class==j)]
     n1<-dim(exp_groupA)[2]
@@ -50,7 +49,7 @@ for (i in 1:num_classes){
   cohen_d[,i]<-min_d
 }
 rownames(cohen_d)<-biomarker_union
-colnames(cohen_d)<-paste0('class',1:10)
+colnames(cohen_d)<-paste0('class',1:num_classes)
 
 s_dscorePerPanel<-rep(0,length(panel_list))
 names(s_dscorePerPanel)<-1:length(panel_list)
@@ -60,5 +59,5 @@ for (i in 1:length(panel_list)){
 }
 
 ############# write the evaluation metrics in csv file #############
-df_metrics_BIM<-data.frame(perfermance_f1score=panel_log$performance,frequency_score=s_freqPerPanel[as.character(1:length(panel_list))],effectSize_score=s_dscorePerPanel[as.character(1:length(panel_list))])
-df_metrics_BIM <- data.frame(size=panel_log$size, perfermance_f1score=panel_log$performance,frequency_score=s_freqPerPanel,effectSize_score=s_dscorePerPanel)
+df_metrics_BIM <- data.frame(size=panel_log$size, BIM_perfermance_f1score=panel_log$performance,BIM_frequency_score=s_freqPerPanel,BIM_effectSize_score=s_dscorePerPanel)
+write.table(df_metrics_BIM,file='../results/BIM_metrics.csv', sep=',',row.names=TRUE, col.names=TRUE,quote=FALSE)
